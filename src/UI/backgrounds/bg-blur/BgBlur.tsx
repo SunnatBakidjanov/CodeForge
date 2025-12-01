6448fdd8f38a0dacae3fc1eff09b09c247456438
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-12-01 13:14:56 +0500

    feat: добавлен цвет в BgBlur
    --BgBlur: добавлен цвет blue-black

--- src/UI/backgrounds/bg-blur/BgBlur.tsx
+++ src/UI/backgrounds/bg-blur/BgBlur.tsx
@@ -7,6 +7,7 @@ const cvaStyle = cva('', {
 	variants: {
 		color: {
 			hotOrange: 'bg-[var(--hot-orange)]',
+			blueBlack: 'bg-[var(--blue-black)]',
 			reactBlue: 'bg-[var(--react-blue)]',
 			htmlOrange: 'bg-[var(--html-orange)]',
 			jsYellow: 'bg-[var(--js-yellow)]',
