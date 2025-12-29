d7327d91855317f013bc5d2c96955d33e39b686d
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-12-29 16:28:54 +0500

    chore: установлен Tanstack Query
    --main.tsx: установлен провайдер и клиент Tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 /* --- Imports --- */
 import { createRoot } from 'react-dom/client';
-import { App } from './app/App.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { App } from './app/App.tsx';
 
 /* --- Create React Root --- */
 const reactRoot = document.getElementById('root') as HTMLElement;
 const rootEleemnt = createRoot(reactRoot);
 
+/* React Query Client */
+const queryClient = new QueryClient();
+
 /* --- Render App --- */
 rootEleemnt.render(
 	<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</GoogleOAuthProvider>
 );
