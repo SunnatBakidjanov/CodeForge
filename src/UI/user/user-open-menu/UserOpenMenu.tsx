780ef421f35c9bbb3b8dbd5e99e10c584864d9d5
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2026-01-09 23:47:53 +0500

    feat: добавлен floating в UserProfileMenu
    --UserProfileMenu: добавлен floating

--- src/UI/user/user-open-menu/UserOpenMenu.tsx
+++ src/UI/user/user-open-menu/UserOpenMenu.tsx
@@ -4,16 +4,25 @@ import { UserBtn } from '../user-btn/UserBtn';
 import { type UserData } from '@/api/useMe';
 import { UserProfileMenu } from '../user-profile-menu/UserProfileMenu';
 import { AnimatePresence } from 'framer-motion';
+import { useFloating, flip, shift, offset } from '@floating-ui/react-dom';
 
 /** --- UserOpenMenu Component --- */
 export const UserOpenMenu = ({ userData }: { userData?: UserData }) => {
 	const [isOpen, setOpen] = useState(false);
+	const { x, y, refs, strategy } = useFloating({
+		placement: 'bottom',
+		middleware: [flip(), offset(0), shift({ padding: 0 })],
+	});
 
 	return (
 		<div className="relative">
-			<UserBtn userData={userData} mainCompProps={{ onClick: () => setOpen(prev => !prev) }} isCloseTooltip={isOpen} />
+			<div ref={refs?.setReference}>
+				<UserBtn userData={userData} mainCompProps={{ onClick: () => setOpen(prev => !prev) }} isCloseTooltip={isOpen} />
+			</div>
 
-			<AnimatePresence>{isOpen && <UserProfileMenu userData={userData} />}</AnimatePresence>
+			<AnimatePresence>
+				{isOpen && <UserProfileMenu userData={userData} floatingOption={{ x, y, strategy, setFloating: refs?.setFloating }} />}
+			</AnimatePresence>
 		</div>
 	);
 };
--- src/UI/user/user-profile-menu/UserProfileMenu.tsx
+++ src/UI/user/user-profile-menu/UserProfileMenu.tsx
@@ -1,25 +1,49 @@
 /** --- Imports --- */
 import { useMe, type UserData, type ResData as UserResData } from '@/api/useMe';
 import { motion } from 'framer-motion';
+import type { Strategy } from '@floating-ui/react-dom';
+
+/* --- Types --- */
+type FloatingOptions = {
+	setFloating: (node: HTMLElement | null) => void;
+	x: number;
+	y: number;
+	strategy: Strategy;
+};
+
+type Props = {
+	floatingOption: FloatingOptions;
+	userData?: UserData;
+};
 
 /** --- UserProfileMenu Component --- */
-export const UserProfileMenu = ({ userData }: { userData?: UserData }) => {
+export const UserProfileMenu = ({ userData, floatingOption }: Props) => {
 	const { data } = useMe({ staleTime: userData ? Infinity : 0 });
+	const { x, y, strategy, setFloating } = floatingOption;
 	const { userData: user } = data as UserResData;
 	const firstLatter = user?.name.charAt(0);
 
 	return (
-		<motion.div
-			className="absolute top-full backdrop-blur-md bg-black/40"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
+		<div
+			ref={setFloating}
+			style={{
+				position: strategy,
+				top: y ?? 0,
+				left: x ?? 0,
+			}}
 		>
-			<div className="flex flex-col">
-				<div className="bg-white/5 rounded-full w-10 h-10 font-bold text-white flex items-center justify-center">{firstLatter}</div>
-				<p>{user?.name}</p>
-				<p>{user?.email}</p>
-			</div>
-		</motion.div>
+			<motion.div
+				className="backdrop-blur-md bg-black/40"
+				initial={{ opacity: 0, scale: 1 }}
+				animate={{ opacity: 1, scale: 0.9 }}
+				exit={{ opacity: 0, scale: 0.8, backdropFilter: 'blur(20px)' }}
+			>
+				<div className="flex flex-col">
+					<div className="bg-white/5 rounded-full w-10 h-10 font-bold text-white flex items-center justify-center">{firstLatter}</div>
+					<p>{user?.name}</p>
+					<p>{user?.email}</p>
+				</div>
+			</motion.div>
+		</div>
 	);
 };
