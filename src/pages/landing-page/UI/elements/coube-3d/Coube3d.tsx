7249d02e8d6d90eec4ea3cdbed97dee3e57393b4
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2026-03-02 20:35:45 +0500

    feat: доработан Coube3d
    --Coube3d: доработано лого 3d

--- src/pages/landing-page/UI/elements/coube-3d/Coube3d.tsx
+++ src/pages/landing-page/UI/elements/coube-3d/Coube3d.tsx
@@ -1,18 +1,61 @@
 import { cn } from '@/utils/cn';
 import style from './index.module.css';
 
+type LogoSides = { side: 'left' | 'right' | 'face' | 'back' };
+type SidesShadow = { shadowSide: 'left' | 'right' };
+type TopSideShadows = { shadowSide: 'leftTop' | 'leftBottom' | 'rightTop' | 'rightBottom' };
+type CoubeConfig = { logoSides: LogoSides[]; sidesShadow: SidesShadow[]; topSideShadows: TopSideShadows[] };
+
+const coubeConfig: CoubeConfig = {
+	logoSides: [{ side: 'left' }, { side: 'right' }, { side: 'face' }, { side: 'back' }],
+	sidesShadow: [{ shadowSide: 'left' }, { shadowSide: 'right' }],
+	topSideShadows: [{ shadowSide: 'leftTop' }, { shadowSide: 'leftBottom' }, { shadowSide: 'rightTop' }, { shadowSide: 'rightBottom' }],
+};
+
 export const Coube3d = () => {
 	return (
 		<div className="w-full">
 			<div className="flex items-center justify-center perspective-midrange">
 				<div className="perspective-midrange transform-3d -rotate-x-14">
-					<div className={cn('relative w-60 h-8 transform-3d', style.animateLogo)}>
-						<div className="absolute inset-0 shadow-white shadow-[0_0_6px] rounded-md translate-z-30.5"></div>
-						<div className="absolute inset-0 shadow-white shadow-[0_0_6px] rounded-md -translate-z-30.5 rotate-y-180"></div>
-						<div className="absolute inset-0 shadow-white shadow-[0_0_6px] rounded-md rotate-y-90 translate-x-30.5"></div>
-						<div className="absolute inset-0 shadow-white shadow-[0_0_6px] rounded-md -rotate-y-90 -translate-x-30.5"></div>
-						<div className="absolute w-60 h-60 shadow-white bg-white/20 shadow-[0_0_6px]  rotate-x-90 -translate-y-30.5"></div>
-						<div className="absolute w-60 h-60 bg-white/20 blur-2xl -rotate-x-90 -translate-y-13"></div>
+					<div className={cn('relative w-60 h-6 transform-3d', style.animateLogo)}>
+						{coubeConfig.logoSides.map(({ side }: LogoSides) => (
+							<div
+								key={side}
+								className={cn(
+									'flex items-center absolute inset-0 overflow-hidden shadow-white bg-(--blue-black) shadow-[0_0_6px] rounded-sm backface-hidden',
+									{
+										'translate-z-30.5': side === 'face',
+										'-translate-z-30.5 rotate-x-180': side === 'back',
+										'rotate-y-90 translate-x-30.5': side === 'left',
+										'-rotate-y-90 -translate-x-30.5': side === 'right',
+									}
+								)}
+							>
+								{coubeConfig.sidesShadow.map(({ shadowSide }: SidesShadow) => (
+									<div
+										className={cn('absolute w-8 h-8 bg-white blur-xl', {
+											'left-0': shadowSide === 'left',
+											'right-0': shadowSide === 'right',
+										})}
+									/>
+								))}
+							</div>
+						))}
+
+						<div className="overflow-hidden absolute w-60 h-60 shadow-white bg-(--blue-black) shadow-[0_0_6px] rounded-sm rotate-x-90 -translate-y-30.5 backface-hidden">
+							{coubeConfig.topSideShadows.map(({ shadowSide }: TopSideShadows) => (
+								<div
+									className={cn('absolute w-8 h-8 bg-white blur-xl', {
+										'left-0 top-0': shadowSide === 'leftTop',
+										'left-0 bottom-0': shadowSide === 'leftBottom',
+										'right-0 top-0': shadowSide === 'rightTop',
+										'right-0 bottom-0': shadowSide === 'rightBottom',
+									})}
+								/>
+							))}
+						</div>
+
+						<div className="absolute w-60 h-60 bg-white/20 blur-lg -rotate-x-90 -translate-y-19"></div>
 					</div>
 				</div>
 			</div>
